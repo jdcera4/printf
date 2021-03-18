ce7bea1d15921ea051a890ceb276f8f01394236a
jdcera4 <jdcera4@gmail.com>
2021-03-18 03:26:14 +0000

    printf

--- holberton.h
+++ holberton.h
@@ -2,7 +2,11 @@
 #define HOLBERTON_H
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
+void func_s(va_list li);
+void func_c(va_list li);
+void func_i(va_list li);q
 
 #endif
